ba6935431c2c0844725f1490b2abe0f9281ad18f
Rocio Fernandez <97984182+ro-fer@users.noreply.github.com>
2024-06-18 10:47:12 -0300

    Delete Unidad 4 directory

--- Unidad 4/filtro_maximo.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = filtro_maximo( input_args )
-%FILTRO_MAXIMO Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
--- Unidad 4/filtro_minimo.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = filtro_minimo( input_args )
-%FILTRO_MINIMO Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
